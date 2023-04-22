478b77551cf1284267cb56319a661a0f242dc5df
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-22 10:30:37 +0300

    Created a map of all the posts and rendered them in html markup format

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -3,6 +3,13 @@ import { useSelector } from 'react-redux'
 const PostsList = () => {
 	const posts = useSelector((state) => state.posts)
 
+	const renderedPosts = posts.map((post) => (
+		<article key={post.id}>
+			<h3>{post.title}</h3>
+			<p>{post.content.substring(0, 100)}</p>
+		</article>
+	))
+
 	return <div>PostsList</div>
 }
 
