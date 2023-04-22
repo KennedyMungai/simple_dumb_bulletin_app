8ee8f7489ec78a35581cb3b8f435736fae3f0723
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-22 10:23:51 +0300

    Created the posts Slice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+	{
+		id: '1',
+		title: 'Learning Redux toolkit',
+		content: 'Chicken wings are the best'
+	},
+	{
+		id: 2,
+		title: 'Slices...',
+		content: 'The more I say slice, the more stuff happens'
+	}
+]
+
+const postsSlice = createSlice({
+	name: 'Posts',
+	initialState,
+	reducers: {}
+})
+
+export default postsSlice.reducer
